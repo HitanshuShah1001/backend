5c5713d3cf633a80e04dea8df4af7544a483a40d
HitanshuShah1001 <hitanshushah5@gmail.com>
2022-07-16 01:46:41 +0530

    Fix:app crash:

--- index.js
+++ index.js
@@ -2,19 +2,20 @@
 
 const express = require('express')
 const articles = require('./articles.js');
-
+require('dotenv').config()
 const app = express();
 
 app.use(express.json())
-app.listen(3000,() => {
+const PORT = process.env.PORT || 443;
+app.listen(PORT ,() => {
     console.log('Server is running');
 })
 
-app.get('/',(res) => {
-    res.send('Running on port 3000')
+app.get('/',(req,res) => {
+    res.send('Running on port 4000')
 })
 
 
 app.get('/article',(req,res) => {
-    res.send(articles,JSON.parse(Object.keys(articles).length))
+    res.send(articles)
 })
